69cb489ef34b9dbd657e3638cf03a69804a22cf0
BaoAsus <your_email_address@example.com>
2022-08-15 06:05:15 +0700

    baotq update some css

--- coffee-shop/src/app/home-page/home-page.component.ts
+++ coffee-shop/src/app/home-page/home-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {GetDishList} from './service/getDishList';
 import {DishWithAmountOrder} from './model/DishWithAmountOrder';
 import {DishWithTimeCreate} from './model/DishWithTimeCreate';
@@ -22,25 +22,37 @@ export class HomePageComponent implements OnInit {
     this.get5DishMostOrder();
     this.get5DishNewest();
   }
-  get5DishMostOrder(){
-    this.getDishList.get5DishMostOrder().subscribe( data => {
+
+  /**
+   * @date 14/08/2022
+   * @author BaoTQ
+   * @Function get 5 dish most order to display on homepage
+   */
+
+  get5DishMostOrder() {
+    this.getDishList.get5DishMostOrder().subscribe(data => {
       this.dishMostOrderList = data;
-      console.log(data)
+      console.log(data);
     }, error => {
       this.checkData = false;
-      this.mess.error('Server have some problem, try reload page','ERROR')
-    })
+      this.mess.error('Máy chủ có thể đãng gặp sự cố, một số thông tin sẽ không thể hiển thị, hãy thử lại sau', 'LỖI');
+    });
   }
 
-  get5DishNewest(){
-    this.getDishList.get5DishNewest().subscribe( data => {
+  /**
+   * @date 14/08/2022
+   * @author BaoTQ
+   * @Function get 5 dish newest to display on homepage
+   */
+
+  get5DishNewest() {
+    this.getDishList.get5DishNewest().subscribe(data => {
       this.distNewestList = data;
-      console.log(data)
+      console.log(data);
     }, error => {
       this.checkData = false;
-      this.mess.error('Server have some problem, try reload page','ERROR')
-    })
+      this.mess.error('Máy chủ có thể đãng gặp sự cố, một số thông tin sẽ không thể hiển thị, hãy thử lại sau', 'LỖi');
+    });
   }
 
 }
-
